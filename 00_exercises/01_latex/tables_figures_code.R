61dc52ff32d95850d1fbc70ae7df7ec226bf136e
leowelma <welmaleo@googlemail.com>
2026-01-13 14:52:27 +0100

    added code provided in the courses to create figures and tables

---
+++ 00_exercises/01_latex/tables_figures_code.R
@@ -0,0 +1,23 @@
+# figures and tables which were provided in the course
+# https://mlarpis.github.io/markup/week%201.html
+
+#load package lattice
+library(lattice)
+library(xtable) # generate the LaTeX code for tables
+#fix the random generator seed
+set.seed(123)
+#create data
+data <- rnorm(1000)
+#plot histogram
+histogram(data)
+#plot density 
+densityplot(data^12 / data^10, xlab = expression(data^12/data^10))
+#plot stripplot
+stripplot(data^2, xlab = expression(data^2))
+#plot boxplot
+bwplot(exp(data))
+#matrix with all data used
+data.all <- cbind(data = data, 
+                  squared1 = data^12 / data^10,
+                  squared2 = data^2,
+                  exponent = exp(data))
